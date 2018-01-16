23ed02475a35545dcf26bd18104da10242978306
Faithlesss <Cr8zyK1LL3rVNZ@gmail.com>
2018-01-16 00:38:15 -0800

    Documented the code with comments

--- error.rb
+++
@@ -1,8 +0,0 @@
-class CoordinateDoesNotExist < StandardError
-end
-
-class AlreadyPlayed < StandardError
-end
-
-class NoAI < StandardError
-end
--- player.rb
+++ player.rb
@@ -9,6 +9,10 @@ end
 
 class Game
 
+# -------------------------------------------------------------
+# Player class that is used to initialize player objects
+# -------------------------------------------------------------
+
 	class Player
 		attr_accessor :name
 
@@ -21,6 +25,12 @@ class Game
 		end
 	end
 
+# -------------------------------------------------------------
+# Initializes the board
+# Makes the player objects based on player input (names)
+# Outputs the board
+# Starts the game
+# -------------------------------------------------------------
 
 	def initialize
 		make_board
@@ -29,17 +39,22 @@ class Game
 		start
 	end
 
+# -------------------------------------------------------------
+# The start function that is used to play the actual game
+# Documentation inside
+# -------------------------------------------------------------
+
 	def start
 		puts "#{@players[0].name} is 'X'\t\t#{@players[1].name} is 'O'\n\n"
-		p1turn = true
-		while(1)
+		p1turn = true # Used to check which player's turn it is. true = P1, false = P2 or AI
+		while(1) # game loop, can only be broken out of if the win condition is met
 			if p1turn == true
 				p1turn = false
 				begin
 					print "[#{@players[0].name}'s turn] Enter a coordinate on the board to make your choice: "
 					choice = gets.chomp
-					raise CoordinateDoesNotExist unless @grid.has_key?(choice.to_sym)
-					raise AlreadyPlayed unless @grid[choice.to_sym] == "null"
+					raise CoordinateDoesNotExist unless @grid.has_key?(choice.to_sym) # Coordinate doesn't exist
+					raise AlreadyPlayed unless @grid[choice.to_sym] == "null" # Coordinate has already been played
 				rescue CoordinateDoesNotExist
 					puts "Coordinate doesn't exist on the board. Please try again."
 					retry
@@ -58,8 +73,8 @@ class Game
 				begin
 					print "[#{@players[1].name}'s turn] Enter a coordinate on the board to make your choice: "
 					choice = gets.chomp
-					raise CoordinateDoesNotExist unless @grid.has_key?(choice.to_sym)
-					raise AlreadyPlayed unless @grid[choice.to_sym] == "null"
+					raise CoordinateDoesNotExist unless @grid.has_key?(choice.to_sym) # See above
+					raise AlreadyPlayed unless @grid[choice.to_sym] == "null" # See above
 				rescue CoordinateDoesNotExist
 					puts "Coordinate doesn't exist on the board. Please try again."
 					retry
@@ -67,7 +82,7 @@ class Game
 					puts "Cannot choose that space as that coordinate has already been played. Please try again."
 					retry
 				end
-				@grid[choice.to_sym] = "O"
+				@grid[choice.to_sym] = "O" 
 				@board.gsub!(choice.to_s, " O")
 				puts @board
 				break if win?
@@ -77,6 +92,10 @@ class Game
 	end
 
 	private
+
+# -------------------------------------------------------------
+# Initializes the board string and the grid hash
+# -------------------------------------------------------------
 
 	def make_board
 		@board = "
@@ -93,13 +112,17 @@ class Game
 						}
 	end
 
+# -------------------------------------------------------------
+# This function is used to create the player objects.
+# -------------------------------------------------------------
+
 	def make_players
 		begin
 			print "Type 1 to play against an AI, or type 2 to play with 2 Players: "
 			@playercount = gets.chomp
 			raise RuntimeError if @playercount.to_i.class.to_s != "Integer"
 			raise RuntimeError if @playercount.to_i < 1 || @playercount.to_i > 2
-			raise NoAI if @playercount.to_i == 1
+			raise NoAI if @playercount.to_i == 1 # Temporary, will be removed when AI has been implemented
 		rescue RuntimeError
 			puts "Input must be a choice of 1 or 2. Please try again."
 			retry
@@ -107,7 +130,7 @@ class Game
 			puts "This version does not have AI capabilities yet. Please pick choice 2."
 			retry
 		end
-		@players = []
+		@players = [] # Array used to store player objects
 
 		for i in 0...@playercount.to_i
 			print "Enter the name of Player#{i+1}: "
@@ -120,6 +143,11 @@ class Game
 			@players << player
 		end
 	end
+
+# -------------------------------------------------------------
+# All the win conditions are checked against by using the grid
+# hash
+# -------------------------------------------------------------
 
 	def win?
 		if(@grid[:a1] == "X" && @grid[:a2] == "X" && @grid[:a3] == "X")
@@ -177,6 +205,11 @@ class Game
 		end
 	end
 
+# -------------------------------------------------------------
+# Used as the end game prompt to check whether the player
+# wants to play again
+# -------------------------------------------------------------
+
 	def play_again
 		begin		
 			print "Would you like to play again?(Y/n): "
@@ -199,3 +232,5 @@ class Game
 	end
 
 end
+
+game = Game.new
